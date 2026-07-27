b1cbc5aee81696d05d4d397fb98256bae0c57140
rabia-iqbal-digital <rabiaiqbaldigital@gmail.com>
2026-07-27 21:36:30 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nutrisnap-ai/', // 👈 Add this line
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
